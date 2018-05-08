1dcb4ac23bbc8031ce8835758ca718f146d59879
zhoujy <anozjy@gmail.com>
2018-05-08 11:34:24 +0800

    删除多余的html文件，客户管理增加表格

--- src/js/customer/index.js
+++ src/js/customer/index.js
@@ -1,13 +1,68 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Table, Button, Pagination } from 'antd'
 
 // 客户管理功能
 class Customer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      userInfo: [{
+        key: '1',
+        name: 'adam',
+        age: '32',
+        telNumber: '13112345678',
+        gender: 'male',
+      },{
+        key: '2',
+        name: 'adam',
+        age: '32',
+        telNumber: '13112345678',
+        gender: 'male',
+      },{
+        key: '3',
+        name: 'adam',
+        age: '32',
+        telNumber: '13112345678',
+        gender: 'male',
+      },{
+        key: '4',
+        name: 'adam',
+        age: '32',
+        telNumber: '13112345678',
+        gender: 'male',
+      }],
+      columns: [{
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name'
+      },{
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age'
+      },{
+        title: '性别',
+        dataIndex: 'gender',
+        key: 'gender'
+      },{
+        title: '手机号码',
+        dataIndex: 'telNumber',
+        key: 'telNumber'
+      },{
+        title: '操作',
+        dataIndex: 'operation',
+        key: 'operation',
+        render: () => <Button type='danger'>删除</Button>
+      }]
+    }
+  }
   render () {
     return (
-      <div>
-        <h1>客户管理功能</h1>
-        <h1>实现对酒店的客户信息的统一管理</h1>
+      <div className='container'>
+        <h1 className='title'>客户管理功能</h1>
+        <div className='pd20'>
+          <Table dataSource={this.state.userInfo} columns={this.state.columns}/>
+        </div>
       </div>
     )
   }
